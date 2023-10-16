3850a4355ec6c7825d5c02b27e8708af5e051a13
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-10-16 12:21:57 +0700

    🔨 refactor(dashboard_view.dart): improve code formatting and structure for better readability 🔨 refactor(login_view.dart): improve code formatting and structure for better readability

--- lib/module/dashboard/view/dashboard_view.dart
+++ lib/module/dashboard/view/dashboard_view.dart
@@ -1,31 +1,40 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_navigation1/core.dart';
 import '../controller/dashboard_controller.dart';
 
 class DashboardView extends StatefulWidget {
-    const DashboardView({Key? key}) : super(key: key);
+  const DashboardView({Key? key}) : super(key: key);
 
-    Widget build(context, DashboardController controller) {
+  Widget build(context, DashboardController controller) {
     controller.view = this;
 
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: const Text("Dashboard"),
         actions: const [],
-        ),
-        body: SingleChildScrollView(
+      ),
+      body: SingleChildScrollView(
         child: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-            children: const [],
-            ),
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Button Back"),
+                ),
+              ),
+            ],
+          ),
         ),
-        ),
+      ),
     );
-    }
+  }
 
-    @override
-    State<DashboardView> createState() => DashboardController();
+  @override
+  State<DashboardView> createState() => DashboardController();
 }
-    
