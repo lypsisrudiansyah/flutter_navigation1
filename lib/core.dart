5ae168d22f55d7941f1c2657bb536d19ffae8053
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-10-16 11:27:34 +0700

    📝 chore(config): add config.json file to project
    📝 chore(core): add core.dart file to project
    
    📝 chore(main): import core.dart and state_util.dart in main.dart
    
    📝 chore(dashboard): add dashboard_controller.dart and dashboard_view.dart files to project
    
    📝 chore(login): add login_controller.dart and login_view.dart files to project
    
    📝 chore(state_util): add state_util.dart file to project
    
    ℹ️ The changes were made to add necessary files and dependencies to the project structure. The config.json file was added to store configuration settings. The core.dart file was added to export necessary classes for the project. The main.dart file was modified to import the core.dart and state_util.dart files. The dashboard_controller.dart and dashboard_view.dart files were added to implement the dashboard module. The login_controller.dart and login_view.dart files were added to implement the login module. The state_util.dart file was added to provide utility functions for managing app state.

---
+++ lib/core.dart
@@ -0,0 +1,9 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:flutter_navigation1/module/dashboard/controller/dashboard_controller.dart';
+export 'package:flutter_navigation1/module/dashboard/view/dashboard_view.dart';
+export 'package:flutter_navigation1/module/login/controller/login_controller.dart';
+export 'package:flutter_navigation1/module/login/view/login_view.dart';
